08dacd4522751d2ab8e2a8cfcaaf32938e04f77f
Julian Posada <julian_2120@hotmail.com>
2024-02-26 22:01:18 -0500

    Aplicacion Pipes Angular

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+
+
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuModule } from 'primeng/menu';
+import { PanelModule } from 'primeng/panel';
+import { ToolbarModule } from 'primeng/toolbar';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+
+@NgModule({
+	exports: [
+		ButtonModule,
+		CardModule,
+		FieldsetModule,
+		MenubarModule,
+		MenuModule,
+		PanelModule,
+		ToolbarModule,
+		InputTextModule,
+		TableModule
+	],
+})
+export class PrimeNgModule {}
